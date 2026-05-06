2aca935614a75d0d9ed39a9eded8a575a9f18915
aandrey <andrey.akimov@abgroup.tech>
2026-05-06 15:09:46 +0300

    feat: add basic auth

---
+++ auth/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# replace with getting from env
+DB_PORT = 5432
+DB_USER = 'user'
+DB_PASSWORD = 'password'
+
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@localhost:{DB_PORT}/zebra"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()

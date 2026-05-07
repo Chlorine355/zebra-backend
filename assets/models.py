6a7788b3dafb43251dc9ce487532e82b8b3ab796
aandrey <andrey.akimov@abgroup.tech>
2026-05-07 12:52:08 +0300

    feat: update asset schema

--- assets/models.py
+++ assets/models.py
@@ -5,6 +5,7 @@ class Asset(Base):
     __tablename__ = "assets"
 
     id = Column(Integer, primary_key=True, index=True)
+    report_id = Column(Integer, index=True)
     user_id = Column(Integer, index=True)
     datetime = Column(DateTime)
     uri = Column(String)

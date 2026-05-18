9ebd872a7cb3a499e198ffc01df19b706ec83df1
Chlorine355 <akimoff.an@gmail.com>
2026-05-18 21:20:35 +0300

    fix: allow cors

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from fastapi import FastAPI
 from auth.routes import router as auth_router
 from users.routes import router as users_router
 from reports.routes import router as reports_router
+from fastapi.middleware.cors import CORSMiddleware
+
 import socket
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(users_router, prefix="/users", tags=["users"])

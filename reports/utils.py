2c210d450abffb38d27c84b2258aceb1cb89a4ba
Chlorine355 <akimoff.an@gmail.com>
2026-06-03 09:32:21 +0300

    fix: add spacer, proportional height

--- reports/utils.py
+++ reports/utils.py
@@ -36,8 +36,10 @@ def generate_pdf(report: ReportFull):
     story.append(Paragraph('Прошу привлечь нарушителя к ответственности.', style))
     for image in report.assets:
         img_data = open(image.uri, "rb").read()
-        img = KeepTogether(Image(BytesIO(img_data), width=10*mm, height=10*mm))
+        img = KeepTogether(Image(BytesIO(img_data), width=30*mm, kind='proportional'))
         story.append(img)
+        story.append(Spacer(1, 1*mm))
+
     story.append(Spacer(1, 2*mm))
     story.append(Paragraph("Заявитель: _____________________", style))
     story.append(Spacer(1, 2*mm))
